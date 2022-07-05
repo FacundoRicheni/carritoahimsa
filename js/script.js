aaaa42bf0d32dcd21a4ed3116ee088d38457a401
facun <facundoricheni@gmail.com>
2022-07-05 16:29:31 -0300

    swal

--- js/script.js
+++ js/script.js
@@ -36,24 +36,18 @@ precioJabon.textContent = "$" + arrayProductos[2].precio;
 //////////////// ALERTS y AGREGAR PRODUCTOS AL ARRAY CARRITO ///////////////////
 
 document.getElementById('boton').onclick = function(){
-    alert("Agregaste " + arrayProductos[0].nombre + " a tu carrito")
+    swal.fire("Agregaste: " + arrayProductos[0].nombre + " a tu carrito")
     arrayCarrito.push(arrayProductos[0]);
-    // const productojson = JSON.stringify(crema);
-    // localStorage.setItem("crema", productojson);
 };   
 
 document.getElementById('boton2').onclick = function(){
-    alert("Agregaste " + arrayProductos[1].nombre + " a tu carrito");
+    swal.fire("Agregaste: " + arrayProductos[1].nombre + " a tu carrito");
     arrayCarrito.push(arrayProductos[1]);
-    // const productojson2 = JSON.stringify(aceite);
-    // localStorage.setItem("aceite", productojson2);
 };
 
 document.getElementById('boton3').onclick = function(){
-    alert("Agregaste: " + arrayProductos[2].nombre + " a tu carrito");
+    swal.fire("Agregaste: " + arrayProductos[2].nombre + " a tu carrito");
     arrayCarrito.push(arrayProductos[2]);    
-    // const productojson3 = JSON.stringify(jabon);
-    // localStorage.setItem("jabon", productojson3);
 };
 
 
@@ -66,35 +60,3 @@ guardar.onclick = function guardarCarrito(){
 };
 
 
-
-// function vaciarCarrito (){
-//     localStorage.clear();
-// }
-
-// const vaciarCarrito = document.getElementById("vaciarCarrito");
-// vaciarCarrito.onclick = vaciarCarrito();
-
-
-// localStorage.setItem("nombre", "crema");
-// localStorage.setItem("precio", 900);
-
-// localStorage.setItem("nombre", "aceite");
-// localStorage.setItem("precio", 2300);
-
-// localStorage.setItem("nombre", "jabon");
-// localStorage.setItem("precio", 700);
-
-
-
-// function guardarDatos(storage){
-//     let user = document.getElementById(emailAdress).value;
-//     let password = document.getElementById(password).value;
-// }
-
-// const usuario = {
-//     "user": user,
-//     "password": password
-// }
-
-// if (storage=== "sessionStorage")
-//     sessionStorage.setItem("user",JSON.stringify(usuario))
